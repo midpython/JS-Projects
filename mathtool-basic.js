0c26bb4a5b697371d203c50e367faebcec636a15
midpython <156398636+midpython@users.noreply.github.com>
2024-05-02 20:55:09 +0800

    Create mathtool-basic.js
    数学辅助工具包basic

---
+++ mathtool-basic.js
@@ -0,0 +1,18 @@
+function power(under,on) {
+    var value;
+    for (var index = 1;index<on;index++) {
+        value = under*under;
+    }
+    return under;
+}
+
+function sqrt(x) {
+    return Math.sqrt(x);
+}
+
+function lg(x) {
+    return Math.log10(x);
+}
+
+// This model is making at 2024/5/2 20:54
+// This model can make log10 power sqrt
